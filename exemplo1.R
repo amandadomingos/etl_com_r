b51a0a9ef3faa78ab63109d4bfcd45e8dffc8309
Amanda Domingos <ardlima4@gmail.com>
2021-04-05 10:37:37 -0300

    Adicionei pacotes

--- exemplo1.R
+++ exemplo1.R
@@ -1,1 +1,13 @@
-summary(iris)
+#### Exercicios disciplina ELT - 2021.1 ####
+
+
+#Installing packages
+install.packages(c("psych", "stargazer", "dplyr", "ggplot2", "ggpubr"))
+
+# Loading packages
+library("psych")
+library("stargazer")
+library("dplyr")
+library("ggplot2")
+library("ggpubr")
+
