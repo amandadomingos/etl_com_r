3190f8b9db16c230c0a5799f72d8c0f7918d490b
Amanda Domingos <ardlima4@gmail.com>
2021-04-05 10:49:47 -0300

    primeiro commit

---
+++ Scripts/exemplo1.R
@@ -0,0 +1,13 @@
+#### Exercicios disciplina ELT - 2021.1 ####
+
+
+#Installing packages
+install.packages(c("psych", "stargazer", "dplyr", "ggplot2", "ggpubr"))
+
+# Loading packages
+library("psych")
+library("stargazer")
+library("dplyr")
+library("ggplot2")
+library("ggpubr")
+
