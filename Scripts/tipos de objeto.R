53a8b4b7bc0eb2fd6a3a0ec46224a3ee874166f7
Amanda Domingos <ardlima4@gmail.com>
2021-04-26 09:18:10 -0300

    aula 2

---
+++ Scripts/tipos de objeto.R
@@ -0,0 +1,12 @@
+#### Aula 2 - ETL com R ####
+
+# Tipos de objetos
+
+# vetor é o objeto mais básico: sequência em uma dimensão
+vetor1 <- c(1,2,3,4,5,6)
+
+is.vector(vetor1)
+typeof(vetor1)
+str(vetor1)
+class(vetor1)
+length(vetor1)
